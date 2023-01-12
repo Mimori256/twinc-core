10392bd48e6ae96cb533198b856802c35e5626d5
Yuzuki Arai <yudukikun5120@gmail.com>
2023-01-12 15:40:16 +0900

    chore: remove `FileReader` API (#1)

--- createICS.ts
+++ createICS.ts
@@ -1,26 +1,18 @@
 import parseCSV from "./parse";
 import kdb from "./kdb.json";
 
-const createICS = (csv: Blob, ifDeadlinesIncluded: boolean) =>
-  new Promise<string>((resolve) => {
-    const reader = new FileReader();
-    reader.readAsText(csv);
-    reader.onload = () => {
-      const fileContent = String(reader.result);
-      const isFromKdBAlt = fileContent.slice(0, 1) === "科";
-      const idList = isFromKdBAlt
-        ? fileContent
-            .split("\n")
-            .filter((x) => x.slice(0, 1) === '"')
-            .map((x) => x.replace('"', ""))
-            .filter((x, i, self) => self.indexOf(x) === i)
-        : fileContent.split("\n").filter((x, i, self) => self.indexOf(x) === i);
-      const output =
-        parseCSV(idList, kdb, ifDeadlinesIncluded, isFromKdBAlt) +
-        `END:VCALENDAR`;
-
-      resolve(output);
-    };
-  });
+const createICS = (fileContent: string, ifDeadlinesIncluded: boolean) => {
+  const isFromKdBAlt = fileContent.slice(0, 1) === "科";
+  const idList = isFromKdBAlt
+    ? fileContent
+        .split("\n")
+        .filter((x) => x.slice(0, 1) === '"')
+        .map((x) => x.replace('"', ""))
+        .filter((x, i, self) => self.indexOf(x) === i)
+    : fileContent.split("\n").filter((x, i, self) => self.indexOf(x) === i);
+  return (
+    parseCSV(idList, kdb, ifDeadlinesIncluded, isFromKdBAlt) + `END:VCALENDAR`
+  );
+};
 
 export { createICS };
