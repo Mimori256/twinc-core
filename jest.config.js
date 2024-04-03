c8363e0b911f0a18bac881ff2a7ee2f0946abf7e
Mimori256 <mimorimori256@gmail.com>
2024-04-03 20:21:09 +0900

    use `@swc/jest` instead of `ts-jest` for faster testing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.ts?$": "@swc/jest",
+  },
 };
