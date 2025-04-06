85bf636dd01849de03c3330faa001d125ba51d4c
Mimori <mimorimori256@gmail.com>
2025-04-06 17:10:04 +0900

    fix

--- createIdList.ts
+++ createIdList.ts
@@ -1,13 +1,14 @@
 export const createIdList = (fileContent: string, isFromKdBAlt: boolean) => {
   const idList = isFromKdBAlt
     ? fileContent
-        .split("\n")
-        .filter((x) => x.slice(0, 1) === '"')
-        .map((x) => x.replace('"', ""))
-        .filter((x, i, self) => self.indexOf(x) === i)
+      .split("\n")
+      .map((x) => x.replace('"', ""))
+      .map((x) => x.split(",")[0])
+      .filter((x) => x !== "科目番号")
+      .filter((x, i, self) => self.indexOf(x) === i)
     : fileContent
-        .split("\n")
-        .filter((x, i, self) => self.indexOf(x) === i)
-        .map((x) => x.replace(/"/g, ""));
+      .split("\n")
+      .filter((x, i, self) => self.indexOf(x) === i)
+      .map((x) => x.replace(/"/g, ""));
   return idList.map((x) => x.trim());
 };
