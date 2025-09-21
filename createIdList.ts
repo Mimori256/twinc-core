dcf48b0c26345053cdbf3a1347c4485a76ac2bc4
Mimori <mimorimori256@gmail.com>
2025-09-21 19:00:01 +0900

    pnpm format

--- createIdList.ts
+++ createIdList.ts
@@ -3,16 +3,16 @@ export const createIdList = (fileContent: string, isFromKdBAlt: boolean) => {
 
   const idList = isFromKdBAlt
     ? fileContent
-      .split("\n")
-      .map((x) => x.split(",")[0])
-      .map(normalize)
-      .filter((x) => x !== "" && x !== "科目番号")
-      .filter((x, i, self) => self.indexOf(x) === i)
+        .split("\n")
+        .map((x) => x.split(",")[0])
+        .map(normalize)
+        .filter((x) => x !== "" && x !== "科目番号")
+        .filter((x, i, self) => self.indexOf(x) === i)
     : fileContent
-      .split("\n")
-      .map(normalize)
-      .filter((x) => x !== "" && x !== "科目番号")
-      .filter((x, i, self) => self.indexOf(x) === i);
+        .split("\n")
+        .map(normalize)
+        .filter((x) => x !== "" && x !== "科目番号")
+        .filter((x, i, self) => self.indexOf(x) === i);
 
   return idList;
 };
